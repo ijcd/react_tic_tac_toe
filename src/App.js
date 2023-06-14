0a9473d57c446df8eb5960c6e47d27228050338a
Ian Duggan <ian.duggan@roll4gravity.com>
2023-06-13 18:30:55 -0700

    We have Xs and Os now

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { useState } from "react";
 
 export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
     const nextSquares = squares.slice();
-    nextSquares[i] = "X";
+    nextSquares[i] = xIsNext ? "X" : "O";
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
   }
 
   return (
